060c04ba58f211ef49b25b9535795d6138007634
VictoorLDS <vlds221005@gmail.com>
2023-08-15 09:31:02 -0300

    Criação Models, falta compra e itenscompra

--- jsbackend/admin.py
+++ jsbackend/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Categoria, Fornecedor, Movel, Tipo_Usuario, Usuario
+
+admin.site.register(Categoria)
+admin.site.register(Fornecedor)
+admin.site.register(Movel)
+admin.site.register(Tipo_Usuario)
+admin.site.register(Usuario)
