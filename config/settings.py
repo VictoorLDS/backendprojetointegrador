b4a376854e5d30aef19c6ab982477cc973ac4af6
VictoorLDS <vlds221005@gmail.com>
2023-09-05 15:57:58 -0300

    Correção Fl0

--- config/settings.py
+++ config/settings.py
@@ -27,18 +27,6 @@ CSRF_TRUSTED_ORIGINS = ["http://localhost:3000", "http://localhost:8000", "https
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4a2*4+nzl$(v2_24=!iadm%b5-_=2-bo)=6ld4=mpyznz=w)nz'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
