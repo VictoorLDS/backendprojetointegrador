95fb3279ef4bd4600c7591f9881d93779488e3d5
VictoorLDS <vlds221005@gmail.com>
2023-09-29 10:05:56 -0300

    Correcção Serializer movel

--- jsbackend/serializers/movel.py
+++ jsbackend/serializers/movel.py
@@ -1,8 +1,32 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SlugRelatedField, CharField
 
 from jsbackend.models import Movel
+from uploader.models import Image
+from uploader.serializers import ImageSerializer
 
 class MovelSerializer(ModelSerializer):
+    capa_attachment_key = SlugRelatedField(
+        source="foto",
+        queryset=Image.objects.all(),
+        slug_field="attachment_key",
+        required=False,
+        write_only=True,
+    )
+    foto = ImageSerializer(required=False, read_only=True)
+    
     class Meta:
         model = Movel
         fields = "__all__"
+
+class MovelDetailSerializer(ModelSerializer):
+    foto = CharField(source="foto.url")
+    class Meta:
+        model = Movel
+        fields = "__all__"
+        depth = 1
+
+class MovelListSerializer(ModelSerializer):
+    foto = CharField(source="foto.url")
+    class Meta:
+        model = Movel
+        fields = ["id", "nome", "quantidade", "preco", "fornecedor", "categoria", "foto"]
