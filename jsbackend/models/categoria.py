d25ef32e8eeab46ffaac553776ff24479fa3d23f
VictoorLDS <vlds221005@gmail.com>
2023-10-25 16:19:37 -0300

    Mudanças Models Categoria e Móveis

--- jsbackend/models/categoria.py
+++ jsbackend/models/categoria.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
-    material = models.CharField(max_length=100)
 
     def __str__(self):
         return self.descricao
--- jsbackend/models/movel.py
+++ jsbackend/models/movel.py
@@ -7,6 +7,7 @@ from jsbackend.models import Categoria, Fornecedor
 class Movel(models.Model):
     nome = models.CharField(max_length=255)
     quantidade = models.IntegerField(default=0,  null=True, blank=True)
+    cor = models.CharField(max_length=100)
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
     fornecedor = models.ForeignKey(Fornecedor, on_delete=models.PROTECT, related_name="móveis")
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="móveis")
